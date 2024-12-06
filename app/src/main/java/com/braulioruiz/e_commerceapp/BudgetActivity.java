14111a3981ad7a684f5e343090b4219825e92e68
braulio.ruiz <braulio.ruiz@outlook.cl>
2024-12-06 09:45:59 -0300

    modified:   app/src/main/java/com/braulioruiz/e_commerceapp/BudgetActivity.java

--- app/src/main/java/com/braulioruiz/e_commerceapp/BudgetActivity.java
+++ app/src/main/java/com/braulioruiz/e_commerceapp/BudgetActivity.java
@@ -12,7 +12,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
-
 import java.util.ArrayList;
 import java.util.List;
 
